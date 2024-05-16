b229f2c1100cb2a8762102176eda3577a77f10b8
Raul Martin <luarmr@gmail.com>
2024-05-15 20:42:02 -0700

    Stability and copy fixes (#1)
    Co-authored-by: Raul Martin <raul@humansignal.com>

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -1,7 +1,7 @@
 import { List, showToast, Toast } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useState, useEffect, useCallback } from "react";
-import { ProjectApiResponse, Workspace } from "../types";
+import { ProjectApiResponse, Workspace, Project } from "../types";
 import { useAPIAccess } from "../context/APIAccessContext";
 import ProjectListItem from "./projectListItem";
 
@@ -11,8 +11,15 @@ export default function ProjectList() {
   const [searchText, setSearchText] = useState<string>("");
   const [workspace, setWorkspace] = useState<string>("");
   const [workspaces, setWorkspaces] = useState<Workspace[]>([{ id: 0, title: "All Projects" }]);
+
   const { data: workspaceData } = useFetch<Workspace[]>(`${appURL}/api/workspaces?page_size=-1`, {
     headers: { Authorization: `Token ${apiToken}` },
+    parseResponse: async (response: Response) => {
+      if (!response.ok) {
+        return null;
+      }
+      return response.json();
+    },
   });
 
   useEffect(() => {
@@ -28,7 +35,7 @@ export default function ProjectList() {
       const params = new URLSearchParams({
         page: page.toString(),
         page_size: pageSize.toString(),
-        include: "id,title,created_at,color,is_draft,is_published,pinned_at",
+        include: "id,title,created_at,color,is_draft,is_published,pinned_at,workspace",
         title: searchText,
       });
 
@@ -74,6 +81,12 @@ export default function ProjectList() {
     setIsDetailLoading(false);
   }, []);
 
+  const getWorkspaceName = (workspaceId: number) => {
+    if (workspace && workspace !== "0") return ""; // When the user selects a workspace in the filter.
+    const projectWorkspace = workspaces.find((ws) => ws.id === workspaceId);
+    return projectWorkspace ? projectWorkspace.title : "";
+  };
+
   return (
     <List
       isLoading={isLoading}
@@ -81,20 +94,25 @@ export default function ProjectList() {
       onSearchTextChange={setSearchText}
       searchBarPlaceholder="Search Projects"
       searchBarAccessory={
-        <List.Dropdown tooltip="Select Workspace" value={workspace} onChange={setWorkspace}>
-          {workspaces.map((ws) => (
-            <List.Dropdown.Item key={ws.id} title={ws.title} value={String(ws.id)} />
-          ))}
-        </List.Dropdown>
+        workspaceData && (
+          <List.Dropdown tooltip="Select Workspace" value={workspace} onChange={setWorkspace}>
+            {workspaces.map((ws) => (
+              <List.Dropdown.Item key={ws.id} title={ws.title} value={String(ws.id)} />
+            ))}
+          </List.Dropdown>
+        )
       }
       onSelectionChange={resetDetailView}
       isShowingDetail={isDetailLoading}
     >
       {data && data.length > 0 ? (
-        data.map((project) => (
+        data.map((project: Project) => (
           <ProjectListItem
             key={project.id}
-            project={project}
+            project={{
+              ...project,
+              workspaceName: getWorkspaceName(project.workspace),
+            }}
             isDetailLoading={isDetailLoading}
             setIsDetailLoading={setIsDetailLoading}
           />
